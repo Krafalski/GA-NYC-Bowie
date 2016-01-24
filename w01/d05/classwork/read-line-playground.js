fc83ff63469bd89b7cd8e2977870c3262df913b2
adamskriger <adamkriger@gmail.com>
2016-01-23 19:41:15 -0500

    Uploading changes after making switch statement

--- w01/d05/classwork/read-line-playground.js
+++ w01/d05/classwork/read-line-playground.js
@@ -1,5 +1,5 @@
 // var rls = require('readline-sync');
-
+//
 // var feeling = rls.question("How are you feeling today?");
 // console.log(feeling);
 //
@@ -9,20 +9,48 @@
 
 var rls = require('readline-sync');
 
+
 var response1 = rls.question("please enter first digit");
-var operation = rls.question("please enter + - * / ")
+var operation = rls.question("please enter + - * / ");
 var response2 = rls.question("please enter second digit");
 var number1 = parseInt(response1);
 var number2 = parseInt(response2);
 
+//
+// if (operation === "+") {
+//
+// console.log(number1 + number2);
+//
+// }
+//
+// if (operation === "-") {
+// console.log(number1 - number2);
+//
+// }
+//
+//  if (operation === "*") {
+//   console.log(number1 * number2);
+// }
+//
+//  if (operation === "/") {
+//   console.log(number1 / number2);
+// }
 
-if (operation === "+") {
-
-console.log(number1 + number2);
-
-}
-
-if (operation === "-") {
-console.log(number1 - number2)
+switch (operation) {
+  case "+" :
+  console.log(number1 + number2);
+  break;
+/////
+case "-" :
+console.log(number1 - number2);
+break;
+///
+case "*" :
+console.log(number1 * number2);
+break;
+///
+case "/" :
+console.log(number1 / number2)
+break;
 
 }
