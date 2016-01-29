2327356cfedcd2a1c16c159506316d342e232a13
Colin Hart <me@colin-hart.com>
2016-01-29 12:35:26 -0500

    jquery examples

--- w02/d05/instructor/jquery/app.js
+++ w02/d05/instructor/jquery/app.js
@@ -5,12 +5,33 @@
 
 
 $(document).ready(function() {
-
-  $('#addHome').removeClass('btn-danger').addClass('btn-success');
-  $('h1').addClass('text-center');
+  var $homeBtn = $('#addHome');
   var newLink = $('<br /><br /><a id="zillow-link" href="http://www.zillow.com">Visit Zillow.com </a>')
   newLink.appendTo('body');
-  $('#zillow-link').attr("target", "_blank");
+  var $zillowLink = $('#zillow-link');
+
+  $homeBtn.removeClass('btn-danger').addClass('btn-success');
+  $('h1').addClass('text-center');
+  $zillowLink.attr("target", "_blank");
   // newLink.eq(2).attr("target", "_blank");
+  console.log($zillowLink.attr("href"))
+
+    $homeBtn.on('click', function(event){
+      console.log(event)
+      console.log(this)
+    })
+
+    // $homeBtn.click(function(evt){
+    //     console.log(evt);
+    //     console.log(this);
+    // });
+
+
+
+
+
+
+
+
 
 })
