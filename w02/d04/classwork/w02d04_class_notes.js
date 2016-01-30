253f77be9e474b173549e236df7204d7f7b3019a
adamskriger <adamkriger@gmail.com>
2016-01-30 09:45:59 -0500

    Updating files from the week

---
+++ w02/d04/classwork/w02d04_class_notes.js
@@ -0,0 +1,7 @@
+// EventTarget.addEventListener()
+//
+// // The EventTarget.addEventListener() method registers the specified listener on the EventTarget it's called on. The event target may be an Element in a document, the Document itself, a Window, or any other object that supports events (such as XMLHttpRequest).
+// //
+
+
+////Code Along////
