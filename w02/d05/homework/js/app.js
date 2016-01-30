cbf5a7032b6451f03d1ad7e9d2cfffab452650fb
Karolin Rafalski <kr2174@caa.columbia.edu>
2016-01-30 10:34:15 -0500

    started homework

---
+++ w02/d05/homework/js/app.js
@@ -0,0 +1,46 @@
+// alert ("connection successful!")
+
+function Cell(){
+	this.attr = attr;
+	this.setState = function (){};
+	this.playable = function (){};
+}
+
+
+
+function Board(){
+	this.arrayofCells =[];
+	this.currentPlayer = " ";
+	this.winner = " ";
+
+	this.startGame = function (){
+
+	}
+
+	this.makePlay = function (position){
+
+	}
+
+	this.checkWin = function () {
+
+	}
+}
+
+
+var game ={
+	gameBoard: {},
+	makeBoard: function (){
+
+	},
+
+	start: function (){
+
+	},
+
+	play: function (position){
+
+	}
+
+
+
+};
