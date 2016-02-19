993a52d378dfe0f00244a7374819735444cf25ea
Jason Seminara <jseminara@gmail.com>
2016-02-19 14:51:07 -0500

    [burger crud] added views and partials

--- w05/d05/instructor/burger-heaven/server.js
+++ w05/d05/instructor/burger-heaven/server.js
@@ -11,15 +11,22 @@ var burgerRoutes = require( path.join(__dirname, '/routes/burgers'));
 var app = express();
 var port = process.env.PORT || 3000;
 
+// static route to public
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 // log
 app.use( logger('dev') );
 
 
+/*Views*/
+app.set('view engine', 'ejs');
 
 
 /*ROUTES*/
 // home route
-app.get('/', (req,res)=>res.send('<h1>YOU GOT DA HOMEPAGE!</h1>'))
+app.get('/', (req,res)=>res.render('pages/home'))
+
 
 // Burgers
 app.use( '/burgers', burgerRoutes)
