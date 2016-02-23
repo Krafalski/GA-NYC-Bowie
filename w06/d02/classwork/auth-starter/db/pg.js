e98294063756a3bfdbf38ca27676d5003426bb82
Colin Hart <me@colin-hart.com>
2016-02-23 07:12:30 -0500

    push authentication starter code

---
+++ w06/d02/classwork/auth-starter/db/pg.js
@@ -0,0 +1,15 @@
+var pg = require('pg');
+var connectionString = "postgres://generalassembly:generalassembly@localhost/sessions_test";
+
+function loginUser(req,res,next) {
+
+}
+
+function createSecure(email, password, callback) {
+
+}
+
+
+function createUser(req, res, next) {
+  
+}
